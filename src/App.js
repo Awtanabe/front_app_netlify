adb12b4577feca4ef26c236518312c70276ec4b6
Awtanabe <panser098@gmail.com>
2021-01-18 12:22:17 +0900

    エンドポイント変更

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://backend-app1109.herokuapp.com/user")
+     .then((data) => {
+       return data.json()
+     }).then((data) => {
+       this.setState({lists: data})
+     })
+    
+  }
+  render() {
+    console.log(this.state.lists)
+    return (
+      <div className="App">
+        <ul>
+         {this.state.lists.map((li) => <li>{li.name}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
